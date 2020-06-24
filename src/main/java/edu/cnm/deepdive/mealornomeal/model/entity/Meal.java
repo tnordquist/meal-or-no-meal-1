e27522f806710d49331f9bee5aceb367480f418d
Paul C <pcutter@cnm.edu>
2020-06-24 11:55:32 -0600

    Began Meal entity class

--- src/main/java/edu/cnm/deepdive/mealornomeal/model/entity/Meal.java
+++ src/main/java/edu/cnm/deepdive/mealornomeal/model/entity/Meal.java
@@ -1,5 +1,32 @@
 package edu.cnm.deepdive.mealornomeal.model.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Meal {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "meal_id", nullable = false, updatable = false)
+  private Long id;
+
+  @Column(name = "name", length = 100, nullable = false)
+  private String name;
+
+  @Column(name = "instruction", length = 5000, nullable = false)
+  private String instruction;
+
+  @Column(name = "prep_time")
+  private Integer prepTime;
+
+  @Column(name = "requirements", length = 200)
+  private String requirements;
+
+  @ManyToOne
 
 }

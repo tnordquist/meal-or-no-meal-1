6562dfa4f26b50b9634073c66cd75dd3f27d452e
Ambar Rodriguez <ambarrodriguez80@yahoo.com>
2020-07-04 00:18:34 -0600

    Added User interface.

---
+++ src/main/java/edu/cnm/deepdive/mealornomeal/model/service/User.java
@@ -0,0 +1,8 @@
+package edu.cnm.deepdive.mealornomeal.model.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface User extends JpaRepository<User,Long> {
+
+}

c30dbfda1494bd316e7578d2717fe353824254e1
Paul C <pcutter@cnm.edu>
2020-07-05 16:11:46 -0600

    Completed Meal repository with two queries

--- src/main/java/edu/cnm/deepdive/mealornomeal/model/service/MealRepository.java
+++ src/main/java/edu/cnm/deepdive/mealornomeal/model/service/MealRepository.java
@@ -5,4 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MealRepository extends JpaRepository <Meal, Long>{
 
+  Iterable<Meal> getAllByNameContainingOrderByNameAsc(String name);
+
+  Iterable<Meal> getAllByCreator_IdOrderByCreator_IdAsc(long creator_id);
+
 }

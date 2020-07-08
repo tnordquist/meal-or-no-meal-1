9ebeb0e0c3d578dbcc6377011b04be43a8cb3bd9
Ambar Rodriguez <ambarrodriguez80@yahoo.com>
2020-07-08 14:50:23 -0600

    Added User Repository.

--- src/main/java/edu/cnm/deepdive/mealornomeal/model/service/User.java
+++ src/main/java/edu/cnm/deepdive/mealornomeal/model/service/User.java
@@ -1,8 +1,12 @@
 package edu.cnm.deepdive.mealornomeal.model.service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface User extends JpaRepository<User,Long> {
+
+  @Query
+  Iterable<User> getAllByNameContainingOrderByNameAsc(String name);
 
 }

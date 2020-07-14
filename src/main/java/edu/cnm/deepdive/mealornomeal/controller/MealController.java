aeb18da9d79ce44fde387c83f4f78c0a3c5688cc
Paul C <pcutter@cnm.edu>
2020-07-14 08:25:54 -0600

    Building our Meal Controller

--- src/main/java/edu/cnm/deepdive/mealornomeal/controller/MealController.java
+++ src/main/java/edu/cnm/deepdive/mealornomeal/controller/MealController.java
@@ -1,5 +1,70 @@
 package edu.cnm.deepdive.mealornomeal.controller;
 
+import edu.cnm.deepdive.mealornomeal.model.entity.Meal;
+import edu.cnm.deepdive.mealornomeal.model.entity.User;
+import edu.cnm.deepdive.mealornomeal.model.service.CalendarRepository;
+import edu.cnm.deepdive.mealornomeal.model.service.IngredientRepository;
+import edu.cnm.deepdive.mealornomeal.model.service.MealRepository;
+import java.security.Principal;
+import java.util.NoSuchElementException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.server.ExposesResourceFor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/meals")
+@ExposesResourceFor(Meal.class)
 public class MealController {
+
+  private final MealRepository mealRepository;
+
+  @Autowired
+  public MealController(MealRepository mealRepository) {
+    this.mealRepository = mealRepository;
+  }
+
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public Iterable<Meal> get() {return mealRepository.getAllByOrderByCreator_IdAsc();}
+
+  @GetMapping(value = "/{id:\\d+}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Meal get(@PathVariable long id) {
+    return mealRepository.findById(id).orElseThrow(NoSuchElementException::new);
+  }
+
+  @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Iterable<Meal> search(@RequestParam(name = "q", required = true) String filter) {
+    return mealRepository.getAllByNameContainingOrderByNameAsc(filter);
+  }
+
+  // TODO Add authentication piece
+  // TODO Set creator to current user
+  @PostMapping(
+      consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Meal> post(@RequestBody Meal meal) {
+    if (meal.getCreator() != null && meal.getCreator().getId() != null) {
+      meal.setCreator()
+    }
+  }
+
+  @PutMapping(value = "/{id:\\d+}",
+  consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public Meal put(@PathVariable long id, @RequestBody Meal meal) {
+    Meal existingMeal = get(id);
+    if (meal.getName() != null) {
+      existingMeal.setName(meal.getName());
+    }
+    return mealRepository.save(existingMeal);
+  }
+
+
 
 }
--- src/main/java/edu/cnm/deepdive/mealornomeal/model/service/MealRepository.java
+++ src/main/java/edu/cnm/deepdive/mealornomeal/model/service/MealRepository.java
@@ -7,6 +7,6 @@ public interface MealRepository extends JpaRepository <Meal, Long>{
 
   Iterable<Meal> getAllByNameContainingOrderByNameAsc(String name);
 
-  Iterable<Meal> getAllByCreator_IdOrderByCreator_IdAsc(long creator_id);
+  Iterable<Meal> getAllByOrderByCreator_IdAsc();
 
 }

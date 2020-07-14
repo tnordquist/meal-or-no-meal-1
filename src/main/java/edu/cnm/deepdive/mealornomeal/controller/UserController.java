09d30fa1edfbb0a564019a397715edefa560f882
Ambar Rodriguez <ambarrodriguez80@yahoo.com>
2020-07-14 15:19:34 -0600

    Updated User.java Added User Service and Controller.

--- src/main/java/edu/cnm/deepdive/mealornomeal/controller/UserController.java
+++ src/main/java/edu/cnm/deepdive/mealornomeal/controller/UserController.java
@@ -2,12 +2,13 @@ package edu.cnm.deepdive.mealornomeal.controller;
 
 import edu.cnm.deepdive.mealornomeal.model.entity.User;
 import edu.cnm.deepdive.mealornomeal.model.service.UserRepository;
+import edu.cnm.deepdive.mealornomeal.model.service.UserService;
 import java.util.Optional;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.server.ExposesResourceFor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 @ExposesResourceFor(User.class)
 public class UserController {
 
-  private final UserRepository userRepository;
+  private final UserService userService;
 
 @Autowired
-  public UserController(UserRepository userRepository) {this.userRepository = userRepository;}
+  public UserController(UserService userService) {this.userService = userService;}
   @GetMapping(value = "/{id:\\d+}", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<User> get(@PathVariable long id) {
-  return ResponseEntity.of(userRepository.getAllByNameContainingOrderByNameAsc(id));
+  return ResponseEntity.of(userService.get(id));
   }
 
   @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)

67015b1d1078286cc67401a7668229bed077de15
Mickie Morlang <mmorlang@gmail.com>
2020-07-14 14:11:51 -0600

    calendar controller

--- src/main/java/edu/cnm/deepdive/mealornomeal/controller/CalendarController.java
+++ src/main/java/edu/cnm/deepdive/mealornomeal/controller/CalendarController.java
@@ -3,17 +3,21 @@ package edu.cnm.deepdive.mealornomeal.controller;
 import edu.cnm.deepdive.mealornomeal.model.entity.Calendar;
 import edu.cnm.deepdive.mealornomeal.model.service.CalendarRepository;
 import edu.cnm.deepdive.mealornomeal.model.service.MealRepository;
-import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.server.ExposesResourceFor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -43,6 +47,29 @@ public class CalendarController{
           ).orElseThrow(NoSuchElementException::new)
       );
     }
-    return null;
+    return ResponseEntity.created(calendar.getHref()).body(calendar);
   }
+
+  @GetMapping(value = "/{id:\\d+}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Calendar get(@PathVariable long id) {
+    return calendarRepository.findById(id).orElseThrow(NoSuchElementException::new);
+  }
+
+  @PutMapping(value = "/{id:\\d+}",
+      consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public Calendar put(@PathVariable long id, @RequestBody Calendar calendar) {
+    Calendar existingCalendar = get(id);
+    if (calendar.getName() != null) {
+      existingCalendar.setName(calendar.getName());
+    }
+    return calendarRepository.save(existingCalendar);
+  }
+
+  @DeleteMapping(value = "/{id:\\d+}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void delete(@PathVariable long id) {
+
+  }
+
+
 }

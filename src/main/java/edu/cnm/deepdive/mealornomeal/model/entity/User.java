271db0a5404b80c865239b66dc19ce10efa92e35
Ambar Rodriguez <ambarrodriguez80@yahoo.com>
2020-07-14 13:56:26 -0600

    Updated User.java Added User Service and Controller.

--- src/main/java/edu/cnm/deepdive/mealornomeal/model/entity/User.java
+++ src/main/java/edu/cnm/deepdive/mealornomeal/model/entity/User.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.mealornomeal.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +12,7 @@ import javax.persistence.Id;
   @SuppressWarnings("JpaDataSourceORMInspection")
   @Entity
   public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id", updatable = false, nullable = false)
@@ -19,6 +21,11 @@ import javax.persistence.Id;
 
     @Column(name = "name", nullable = false, unique = true)
     private String name;
+
+    @JsonIgnore
+    @Column(nullable = false, updatable = false, unique = true)
+    private String oauthKey;
+
 
     public Long getId() {
       return id;
@@ -30,5 +37,13 @@ import javax.persistence.Id;
 
     public void setName(String name) {
       this.name = name;
+    }
+
+    public String getOauthKey() {
+      return oauthKey;
+    }
+
+    public void setOauthKey(String oauthKey) {
+      this.oauthKey = oauthKey;
     }
   }

ab17246ccbf29fec5598d96169c7729c1df6da82
Ambar Rodriguez <ambarrodriguez80@yahoo.com>
2020-07-15 14:17:12 -0600

    Added UserController.

--- src/main/java/edu/cnm/deepdive/mealornomeal/controller/view/FlatUser.java
+++ src/main/java/edu/cnm/deepdive/mealornomeal/controller/view/FlatUser.java
@@ -1,9 +1,14 @@
 package edu.cnm.deepdive.mealornomeal.controller.view;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.springframework.lang.NonNull;
+
+@JsonPropertyOrder({"id, name, oauthKey "})
 public interface FlatUser {
 
   Long getId();
 
+  @NonNull
   String getName();
 
   String getOauthKey();

17b0dc3f9d96f8a524331a13788f6f4e1685db6e
levisanchez <levijames@gmail.com>
2020-07-15 13:42:20 -0600

    Bug fix for ListItem and Ingredient

--- src/main/java/edu/cnm/deepdive/mealornomeal/controller/IngredientController.java
+++ src/main/java/edu/cnm/deepdive/mealornomeal/controller/IngredientController.java
@@ -8,8 +8,12 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.server.ExposesResourceFor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,24 +40,29 @@ public class IngredientController {
     this.listRepository = listRepository;
   }
 
-//  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-//  public Iterable<Ingredient> get() {
-//    return ingredientRepository.getAllByNameContainingOrderByNameAsc();}
-
-//  @PostMapping(
-//      consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-//  public ResponseEntity<Ingredient> post(@RequestBody Ingredient ingredient, Authentication
-//  )
-
   @GetMapping(value = "{id:\\d+}", produces = MediaType.APPLICATION_JSON_VALUE)
   public Ingredient get(@PathVariable long id) {
     return ingredientRepository.findById(id).orElseThrow((NoSuchElementException::new));
   }
-
-  @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
-  public Iterable<Ingredient> search(@RequestParam(name = "q", required = true) String filter){
-    return ingredientRepository.getAllByNameContainingOrderByNameAsc(filter);
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public Iterable<Ingredient> search(RequestParam(){
+    return ingredientRepository.getAllByNameContainingOrderByNameAsc();
   }
+
+//  @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+//  public Iterable<Ingredient> search(@RequestParam(name = "q", required = true) String filter){
+//    return ingredientRepository.getAllByNameContainingOrderByNameAsc(filter);
+//  }
+
+  @PostMapping(
+      consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Ingredient> postIngredientName(@RequestBody Ingredient ingredient) {
+    if (ingredient.getName() != null && ingredient.getId() != null){
+      ingredient.setName(mealRepository;
+    }
+    return ResponseEntity.created(ingredient.getHref()).body(ingredient);
+  }
+
 
 //  @PutMapping(value = "/id:\\d+}/listItem",
 //  consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
